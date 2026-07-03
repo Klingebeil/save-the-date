00ce99fd2ad4b2c3c451f2dfc62e6b82c3700f25
Johannes Klingebiel <johannes.klingebiel@gmail.com>
2026-07-03 15:18:47 +0200

    link update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <!-- script.js -->
-const SHARED_URL = "https://open.spotify.com/playlist/5xnZNclTNr6mwLpzTuIZ2C?si=d97a67ca4e76454c&pt=99854290b6306df02602d75d72b8c863";
+const SHARED_URL = "https://open.spotify.com/playlist/5xnZNclTNr6mwLpzTuIZ2C?si=8994bbe867da4ea7&pt=1b0f06a6e9d1b472a7020c0bf9769b5b";
 
 document.querySelectorAll(".shared-link").forEach(el => {
   el.href = SHARED_URL;
